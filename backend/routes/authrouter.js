b3a9785c3643230c43aca71601c2afa5b70af40e
Azhar <kazhar136@gmail.com>
2025-02-22 16:10:59 +0530

    update

--- backend/routes/authrouter.js
+++ backend/routes/authrouter.js
@@ -3,6 +3,7 @@ const router = express.Router(); // ✅ Correct way to initialize the router
 const User = require('../models/user'); // Adjust path as needed
 const { signupvalidation, loginpvalidation } = require('../middlewares/authvalidation');
 const { signup, login } = require('../controllers/authcontroller');
+const a=1;
 
 
 
