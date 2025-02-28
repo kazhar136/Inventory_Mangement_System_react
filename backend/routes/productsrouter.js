31fa33ab72d44edcf6117af7561627a0a1bcd7ed
Azhar <kazhar136@gmail.com>
2025-03-01 02:44:01 +0530

    backend update

--- backend/routes/productsrouter.js
+++ backend/routes/productsrouter.js
@@ -1,19 +1,22 @@
 const router = require('express').Router();
 const ensureAuthenticated = require("../middlewares/auth")
 
-router.get('/', ensureAuthenticated, async (req, res) => {
-    try {
-        console.log('✅ Logged user:', req.user);
-        const products = [
-            { name: "mobile", price: 10000 },
-            { name: "tv", price: 4000 }
-        ];
-        res.status(200).json({ success: true, products });
 
-    } catch (error) {
-        res.status(500).json({ success: false, message: "Error fetching products" });
+
+router.get('/', ensureAuthenticated, (req, res) => {
+    console.log('logged user details--------',req.user)
+    res.status(200).json([
+    { 
+        name: "mobile",
+        price: 10000
+    },
+    { 
+        name: "tv",
+        price: 4000
     }
+    ])
 });
+
 
 
 module.exports = router;
